6962b7ae33b1596ffe55372383e886b2999432cb
delafrouz <del.mirfendereski@gmail.com>
2024-03-23 10:37:37 +0330

    Fix the fun regex

--- fun/configs/states.py
+++ fun/configs/states.py
@@ -1,2 +1,4 @@
-FUN = chr(40)
+# characters 40 and 41 are '(' and ')' and making a regex out of them will return
+#   re.error: missing ), unterminated subpattern at position 1
+FUN = chr(42)
 FUN_MEME, FUN_JOKE = map(chr, range(400, 402))
